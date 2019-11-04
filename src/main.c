5a13dc0776ad3e45c05e5fde229835d22d2eec94
Lab ESPL Stud14 lab_espl_stud14 <lab_espl_stud14@tueircs-ldb11.int.rcs.ei.tum.de>
2019-11-04 17:25:57 +0100

    New Main.c added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
 int main() {
-	printf("Hello ESPL\n");
+	
+	
+	do {
+	
+	int num=0;
+	char word[50]= "" ,tik='n';
+
+	printf("Please insert a number as an input! \n");
+	scanf("%d", &num);
+	
+	word = num_to_words(num);
+	printf("This is the returned string: %s \n",word);
+
+	printf("Do you want to try it again? [y]or[n] \n");
+	scanf("%c", &tik);
+
+	} while( tik == 'y' )
+
+	
 	return 0;
 }
